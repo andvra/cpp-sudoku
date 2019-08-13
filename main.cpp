b675a7cfde42af5870034628ffc65263ba8ec6ba
Andreas Vrålstad <andreas.vralstad@gmail.com>
2019-08-13 15:38:28 +0200

    solve a random board

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <bitset>
 #include <chrono>
 #include <thread>
+#include <random>
+#include <iterator>
 #include "board.h"
 #include "threadpool.h"
 
@@ -14,14 +16,32 @@ static void FindSolution(Board &board)
 	board.GenerateSolution();
 }
 
+template <typename Iter, typename RandomGenerator>
+Iter select_randomly(Iter start, Iter end, RandomGenerator &g)
+{
+	uniform_int_distribution<> dis(0, distance(start, end) - 1);
+	advance(start, dis(g));
+	return start;
+}
+
+template <typename Iter>
+Iter select_randomly(Iter start, Iter end)
+{
+	// https://stackoverflow.com/a/16421677
+	static random_device rd;
+	static mt19937 gen(rd());
+	string board = select_randomly(start, end, gen);
+}
+
 int main()
 {
-	string board1 = "870090240106520080094000000000340610001206300063075000000000850020051704057060093";
-	string board2 = "070940520009003007350721090700000000504302806000000002040615089900800200013097040";
-	string board3 = "609007000100200000070063000700006032040000050980100007000820010000005009000600208";
-	string board4 = "000801000000000430500000000000070800000000100020030000600000075003400000000200600";
+	const vector<string> boards = {"870090240106520080094000000000340610001206300063075000000000850020051704057060093",
+								   "070940520009003007350721090700000000504302806000000002040615089900800200013097040",
+								   "609007000100200000070063000700006032040000050980100007000820010000005009000600208",
+								   "000801000000000430500000000000070800000000100020030000600000075003400000000200600"};
 	ThreadPool tp(maxNoThreads);
-	Board board(board4, &tp);
+	string boardAsText = *select_randomly(boards.begin(), boards.end());
+	Board board(boardAsText, &tp);
 	auto t_start = chrono::high_resolution_clock::now();
 	thread t(FindSolution, ref(board));
 	t.detach();
